7e7f4398d876832aae9feeb7af604c40021dd562
sneider2016 <sneiider1999@gmail.com>
2026-07-25 15:26:05 -0500

    Update analyzer.py

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -1,1 +1,13 @@
+class Analyzer:
 
+    def calcular_probabilidad(self, datos):
+        return 0
+
+    def calcular_confianza(self, datos):
+        return 0
+
+    def calcular_ev(self, cuota, probabilidad):
+        return 0
+
+    def detectar_alertas(self, datos):
+        return []
