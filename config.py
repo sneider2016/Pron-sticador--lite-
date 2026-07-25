1e45e7342d7056519fc5af88e14ff30f67f64ac7
sneider2016 <sneiider1999@gmail.com>
2026-07-25 15:04:21 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# ==========================================
+# PRONOSTICADOR ÉLITE PROFESIONAL
+# Motor de Inteligencia Deportiva IA SneiderMompi (SALM)
+# Configuración General
+# ==========================================
+
+API_KEY = "TU_API_KEY"
+
+HOST = "v3.football.api-sports.io"
+
+APP_NAME = "Pronósticador Élite Profesional"
+
+ENGINE_NAME = "Motor de Inteligencia Deportiva IA SneiderMompi"
+
+ENGINE_SHORT = "SALM"
+
+VERSION = "2.0"
