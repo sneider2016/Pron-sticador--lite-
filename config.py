d2096a8e0f3cabf3458a065ac3900917e0ec5e10
sneider2016 <sneiider1999@gmail.com>
2026-08-07 16:54:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # ==========================================
-# PRONOSTICADOR ÉLITE PROFESIONAL
+# PRONOSTICADOR ÉLITE PROFESIONAL SNEIDER
 # Motor de Inteligencia Deportiva IA SneiderMompi (SALM)
 # ==========================================
 
@@ -9,7 +9,7 @@ import os
 API_KEY = "3e69e51ac95c094a672f790edac978b0"
 HOST = "v3.football.api-sports.io"
 
-APP_NAME = "Pronósticador Élite Profesional"
+APP_NAME = "Pronósticador Élite Sneider Profesional"
 ENGINE_NAME = "Motor de Inteligencia Deportiva IA SneiderMompi"
 ENGINE_SHORT = "SALM"
 VERSION = "4.0 Surgeon Elite"
